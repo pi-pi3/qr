3368ccda996136cd3107143956ba3b35275d3356
Szymon Walter <walter.szymon.98@gmail.com>
2018-02-10 15:08:51 +0100

    Add bench test with 8 attributes (rgb, normal, uv)

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ mod tests {
     use image::{ImageBuffer, Pixel, Rgb};
 
     #[cfg(feature = "nightly")]
-    use test::Bencher;
+    use test::{black_box, Bencher};
 
     use super::{Rectangle, Triangle};
 
@@ -273,5 +273,107 @@ mod tests {
         });
 
         image.save("bench3.png").expect("couldn't save image");
+    }
+
+    #[cfg(feature = "nightly")]
+    #[bench]
+    fn attr8_tri(b: &mut Bencher) {
+        let dot = |(ax, ay, az), (bx, by, bz)| ax*bx + ay*by + az*bz;
+        let rand = |m| (rand::random::<f64>() * m, rand::random::<f64>() * m, rand::random::<f64>() * m);
+
+        const BPP: usize = 24;
+        const WIDTH: usize = 1024;
+        const HEIGHT: usize = 1024;
+        const SIZE: usize = WIDTH * HEIGHT * BPP / 8;
+
+        let buffer = vec![0_u8; SIZE];
+        let image = ImageBuffer::from_raw(WIDTH as _, HEIGHT as _, buffer);
+        let mut image = image.unwrap();
+
+        let width = WIDTH as f64 - 1.0;
+        let height = HEIGHT as f64 - 1.0;
+
+        // light normal
+        let (lx, ly, lz) = rand(1.0);
+        let len_recip = (lx*lx + ly*ly + lz*lz).sqrt().recip();
+        let (lx, ly, lz) = (lx * len_recip, ly * len_recip, lz * len_recip);
+
+        b.iter(|| {
+            // position
+            let (x0, x1, x2) = rand(width as f64);
+            let (y0, y1, y2) = rand(height as f64);
+
+            // color
+            let (r0, r1, r2) = rand(255.0);
+            let (g0, g1, g2) = rand(255.0);
+            let (b0, b1, b2) = rand(255.0);
+
+            // normal
+            let (nx0, nx1, nx2) = rand(1.0);
+            let (ny0, ny1, ny2) = rand(1.0);
+            let (nz0, nz1, nz2) = rand(1.0);
+
+            // normalize normal
+            let len_recip0 = (nx0*nx0 + ny0*ny0 + nz0*nz0).sqrt().recip();
+            let len_recip1 = (nx1*nx1 + ny1*ny1 + nz1*nz1).sqrt().recip();
+            let len_recip2 = (nx2*nx2 + ny2*ny2 + nz2*nz2).sqrt().recip();
+
+            let nx0 = nx0 * len_recip0;
+            let nx1 = nx1 * len_recip1;
+            let nx2 = nx2 * len_recip2;
+
+            let ny0 = ny0 * len_recip0;
+            let ny1 = ny1 * len_recip1;
+            let ny2 = ny2 * len_recip2;
+
+            let nz0 = nz0 * len_recip0;
+            let nz1 = nz1 * len_recip1;
+            let nz2 = nz2 * len_recip2;
+
+            // texture coordinate
+            let (tu0, tu1, tu2) = rand(1.0);
+            let (tv0, tv1, tv2) = rand(1.0);
+
+            let v1 = (x0, y0);
+            let v2 = (x1, y1);
+            let v3 = (x2, y2);
+
+            let triangle = Triangle::with_points([v1, v2, v3]);
+
+            triangle
+                .into_iter()
+                .map(|((x, y), b)| ((x as u32, y as u32), b))
+                .for_each(|((x, y), p)| {
+                    // diffuse color at (x, y)
+                    let r = p[0] * r0 + p[1] * r1 + p[2] * r2;
+                    let g = p[0] * g0 + p[1] * g1 + p[2] * g2;
+                    let b = p[0] * b0 + p[1] * b1 + p[2] * b2;
+
+                    // normal at (x, y)
+                    let nx = p[0] * nx0 + p[1] * nx1 + p[2] * nx2;
+                    let ny = p[0] * ny0 + p[1] * ny1 + p[2] * ny2;
+                    let nz = p[0] * nz0 + p[1] * nz1 + p[2] * nz2;
+
+                    // texture uv at (x, y)
+                    let tu = p[0] * tu0 + p[1] * tu1 + p[2] * tu2;
+                    let tv = p[0] * tv0 + p[1] * tv1 + p[2] * tv2;
+
+                    // calculate reflectance according to the lambertian cosine law
+                    let shade = -dot((-lx, -ly, -lz), (nx, ny, nz));
+                    let r = r * shade;
+                    let g = g * shade;
+                    let b = b * shade;
+
+                    // simulate sampling texture
+                    let width = 1024;
+                    let height = 1024;
+                    let index = (width as f64 * tu) as usize + (height as f64 * tv) as usize * width;
+                    let _index = black_box(index);
+
+                    image.put_pixel(x, y, Rgb::from_channels(r as u8, g as u8, b as u8, 255));
+                });
+        });
+
+        image.save("bench4.png").expect("couldn't save image");
     }
 }
